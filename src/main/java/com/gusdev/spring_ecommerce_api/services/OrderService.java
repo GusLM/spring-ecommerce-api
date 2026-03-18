d1a10b746f463ff2b21270b029c4af59ca2d43ca
Gustavo Santos <gustavolimarma@gmail.com>
2026-03-18 00:12:36 -0300

    feat: Order exception handling - findById

--- src/main/java/com/gusdev/spring_ecommerce_api/services/OrderService.java
+++ src/main/java/com/gusdev/spring_ecommerce_api/services/OrderService.java
@@ -2,13 +2,13 @@ package com.gusdev.spring_ecommerce_api.services;
 
 import com.gusdev.spring_ecommerce_api.entities.Order;
 import com.gusdev.spring_ecommerce_api.repositories.OrderRepository;
+import com.gusdev.spring_ecommerce_api.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -24,10 +24,6 @@ public class OrderService {
 
     public Order findById(Long id) {
         Optional<Order> obj = repository.findById(id);
-        if (obj.isPresent()){
-            return obj.get();
-        } else {
-            throw new NoSuchElementException();
-        }
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 }
